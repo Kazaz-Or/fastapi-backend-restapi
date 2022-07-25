066df40a273fd16ac5936214f3775a740c28cfbc
Kazaz-Or <83350680+Kazaz-Or@users.noreply.github.com>
2022-07-25 22:32:14 +0300

    Add endpoint for POST todos + tests

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Depends
 
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from typing import Optional
 
 import models
 from db import engine, SessionLocal
@@ -19,6 +21,13 @@ def get_db():
         db.close()
 
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="The priority must be between 1-5")
+    complete: bool
+
+
 @app.get('/todos')
 async def get_all_todos(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
@@ -30,3 +39,20 @@ async def get_todo(todo_id: int, db: Session = Depends(get_db)):
     if todo_model is not None:
         return todo_model
     raise http_exception()
+
+
+@app.post('/todo', status_code=201)
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        'transaction': 'Successful'
+    }
---
+++ tests/test_post_todos.py
@@ -0,0 +1,182 @@
+import json
+
+import requests
+import random
+
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+from tests import schemas
+
+
+def test_post_todo_status_code():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": 1,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_by_id_content_type_header_response_value():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": 1,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo", data=payload)
+
+    assert response.headers["Content-Type"] == 'application/json'
+
+
+def test_post_todo_missing_optional_field_description():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "priority": "1",
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_missing_required_field_title():
+    payload = json.dumps({
+        "description": "continuous learning",
+        "priority": 1,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 422
+
+
+def test_post_todo_missing_required_field_priority():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 422
+
+
+def test_post_todo_missing_required_field_complete():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": 1,
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 422
+
+
+def test_post_todo_autoconvert_to_the_expected_type_priority_string():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": "1",
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_autoconvert_to_the_expected_type_complete_string():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": 1,
+        "complete": "False"
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_autoconvert_to_the_expected_type_title_int():
+    payload = json.dumps({
+        "title": 213,
+        "description": "continuous learning",
+        "priority": 1,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_special_special_chars():
+    payload = json.dumps({
+        "title": '!@#$%^&*(*)_+',
+        "description": "±§/.']}{[-=",
+        "priority": 1,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_special_spaces():
+    payload = json.dumps({
+        "title": ' ',
+        "description": " ",
+        "priority": 1,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 201
+    assert response.text == '{"status":201,"transaction":"Successful"}'
+
+
+def test_post_todo_priority_less_than_1():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": 0,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 422
+    assert response.reason == 'Unprocessable Entity'
+
+
+def test_post_todo_priority_greater_than_5():
+    payload = json.dumps({
+        "title": "Focus on backend & automation testing",
+        "description": "continuous learning",
+        "priority": 6,
+        "complete": False
+    })
+    response = requests.post(url="http://localhost:8000/todo",
+                             data=payload)
+
+    assert response.status_code == 422
+    assert response.reason == 'Unprocessable Entity'
